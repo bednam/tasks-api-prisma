cf049748ed81d2cdf9e7c63b7bc6e10e672da6b3
Michał Bednarz <mibed90@gmail.com>
2019-08-21 15:41:55 +0200

    Add ms option to aggregate function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
 import * as moment from 'moment'
 
 export const dateToTime = date =>
-	moment.utc(date.as('milliseconds')).format('HH:mm:ss')
+	moment.utc(dateToMilliseconds(date)).format('HH:mm:ss')
+
+export const dateToMilliseconds = date => date.as('milliseconds')
 
 /**
 	@return aggregated duration of specific property of items
@@ -21,9 +23,15 @@ export const aggregateTime = (items, extractTime) =>
 	@param items array of elements containing two properties which difference is to aggregate
 	@param extractStart function which takes single element from items array and returns its property used for aggregation
 	@param extractEnd function which takes single element from items array and returns its property used for aggregation
+	@param momentFormat function which takes final computed moment object and formats it for example to HH:mm:ss or ms @default dateToTime
 */
-export const aggregateTimeDiff = (items, extractStart, extractEnd) =>
-	dateToTime(
+export const aggregateTimeDiff = (
+	items,
+	extractStart,
+	extractEnd,
+	momentFormat = dateToTime
+) =>
+	momentFormat(
 		items.reduce(
 			(acc, curr) =>
 				acc.add(
